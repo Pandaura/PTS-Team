4f92cf45fc86e7be50487fd0d3397ca22073e170
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-08-24 20:11:53 +0200

    Update start.sh

--- menu/functions/start.sh
+++ menu/functions/start.sh
@@ -25,6 +25,8 @@ downloadpg() {
   rm -rf /opt/plexguide
   git clone https://github.com/MrDoobPG/PGBlitz.com.git /opt/plexguide && cp /opt/plexguide/menu/interface/alias/templates/pts /bin/
   cp /opt/plexguide/menu/interface/alias/templates/pts /bin/pts
+  chown -cR 1000:1000 /opt/plexguide/ 1>/dev/null 2>&1
+  chmod -R 775 /opt/plexguide 1>/dev/null 2>&1
 }
 
 missingpull() {
