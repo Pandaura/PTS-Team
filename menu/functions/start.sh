0ff4ab46ee2a075f93da6e11dc0087a2edcd048b
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-09-08 00:16:57 +0200

    small fixxed

--- menu/functions/start.sh
+++ menu/functions/start.sh
@@ -25,7 +25,6 @@ downloadpg() {
   rm -rf /opt/plexguide
   git clone --single-branch https://github.com/MrDoobPG/PGBlitz.com.git /opt/plexguide  1>/dev/null 2>&1
   ansible-playbook /opt/plexguide/menu/alias/alias.yml  1>/dev/null 2>&1
-
 }
 
 missingpull() {
