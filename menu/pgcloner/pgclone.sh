4168c6f8e192ba63782cae076f0da5b3499d6676
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-09-08 01:37:43 +0200

    Update pgclone.sh

--- menu/pgcloner/pgclone.sh
+++ menu/pgcloner/pgclone.sh
@@ -7,14 +7,14 @@
 ################################################################################
 
 ### FILL OUT THIS AREA ###
-echo 'pgclone' >/var/plexguide/pgcloner.rolename
-echo 'PG Clone' >/var/plexguide/pgcloner.roleproper
-echo 'PGClone' >/var/plexguide/pgcloner.projectname
-echo 'v8.6' >/var/plexguide/pgcloner.projectversion
+echo 'rclone' >/var/plexguide/pgcloner.rolename
+echo 'rClone' >/var/plexguide/pgcloner.roleproper
+echo 'rClone' >/var/plexguide/pgcloner.projectname
+echo 'master' >/var/plexguide/pgcloner.projectversion
 echo 'pgclone.sh' >/var/plexguide/pgcloner.startlink
 
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-echo "💬 PG Clone utilizes RClone's Mounts + MergerFS's Union" >/var/plexguide/pgcloner.info
+echo "💬 rClone utilizes RClone's Mounts + MergerFS's Union" >/var/plexguide/pgcloner.info
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 ### START PROCESS
