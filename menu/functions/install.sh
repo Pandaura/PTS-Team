7a56f1f0d7af38137d5400f327f23ffcc088050f
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-09-11 19:59:28 +0200

    role update

--- menu/functions/install.sh
+++ menu/functions/install.sh
@@ -324,36 +324,7 @@ bash /opt/plexguide/menu/roles/pythonstart/pyansible.sh >/dev/null 2>&1
 }
 
 dockerinstall() {
-  ospgversion=$(cat /var/plexguide/os.version)
-  if [ "$ospgversion" == "debian" ]; then
-    ansible-playbook /opt/plexguide/menu/pg.yml --tags dockerdeb
-  else
     ansible-playbook /opt/plexguide/menu/pg.yml --tags docker
-    # If Docker FAILED, Emergency Install
-    file="/usr/bin/docker"
-    if [ ! -e "$file" ]; then
-      clear
-      echo "Installing Docker the Old School Way - (Please Be Patient)"
-      sleep 2
-      clear
-      curl -fsSL get.docker.com -o get-docker.sh
-      sh get-docker.sh
-      echo ""
-      echo "Starting Docker (Please Be Patient)"
-      sleep 2
-      systemctl start docker
-      sleep 2
-    fi
-
-    ##### Checking Again, if fails again; warns user
-    file="/usr/bin/docker"
-    if [ -e "$file" ]; then
-      sleep 5
-    else
-      echo "INFO - FAILED: Docker Failed to Install! Exiting PGBlitz!"
-      exit
-    fi
-  fi
 }
 
 serverid() {
