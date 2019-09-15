a2952c1413cd99daaa14dde9a38b91e9c08664a7
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-09-15 11:27:15 +0200

    mergerfs - / masterupdate roles

---
+++ menu/gce/functions/main.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# Title:      PGBlitz (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://pgblitz.com - http://github.pgblitz.com
+# GNU:        General Public License v3.0
+################################################################################
+
+### assists in creating default variables if they do not exist
+variable() {
+  file="$1"
+  if [ ! -e "$file" ]; then echo "$2" >$1; fi
+}
+
+### key variable pull
+variablepull() {
+
+  ### sets variables if they do not exist
+  variable /var/plexguide/project.account NOT-SET
+  variable /var/plexguide/project.ipaddress NOT-SET
+  variable /var/plexguide/project.ipregion NOT-SET
+  variable /var/plexguide/project.ipzone NOT-SET
+  variable /var/plexguide/project.processor 4 
+  variable /var/plexguide/project.ram 8
+  variable /var/plexguide/project.nvme 1
+  variable /var/plexguide/project.id NOT-SET
+  variable /var/plexguide/project.switch off
+
+  ### variables being called
+
+  #account=$(cat /var/plexguide/project.account)
+  account=$(gcloud config configurations list | tail -n 1 | awk '{print $3}')
+  if [[ "$account" != "" ]]; then echo $account >/var/plexguide/project.account; fi
+
+  ipaddress=$(cat /var/plexguide/project.ipaddress)
+  ipregion=$(cat /var/plexguide/project.ipregion)
+  ipzone=$(cat /var/plexguide/project.ipzone)
+  nvmecount=$(cat /var/plexguide/project.nvme)
+  ramcount=$(cat /var/plexguide/project.ram)
+  processor=$(cat /var/plexguide/project.processor)
+
+  # if user switches usernames, this turns on. turns of when user sets project again
+  switchcheck=$(cat /var/plexguide/project.switch)
+  if [[ "$switchcheck" != "on" ]]; then
+    projectid=$(gcloud config configurations list | tail -n 1 | awk '{print $4}')
+    if [[ "$projectid" != "" ]]; then echo $projectid >/var/plexguide/project.id; fi
+  fi
+
+  serverstatus=serverstatus
+  sshstatus=notready
+}
+
+servercheck() {
+
+  gcedeployedcheck="NOT DEPLOYED"
+  minicheck=$(cat /var/plexguide/project.id)
+  if [[ "$minicheck" != "NOT-SET" ]]; then
+
+    temp55=$(gcloud compute instances list | grep pg-gce)
+    if [[ "$temp55" != "" ]]; then gcedeployedcheck="DEPLOYED"; fi
+
+  fi
+}
---
+++ menu/gce/gcechecker.sh
@@ -0,0 +1,150 @@
+	#!/bin/bash
+	#
+# Title:      PGBlitz (Reference Title File)
+# Author(s):  Admin9705 - Deiteq
+# URL:        https://pgblitz.com - http://github.pgblitz.com
+# GNU:        General Public License v3.0
+################################################################################
+
+	### NOTE THIS IS JUST A COPY - MAIN ONE SITE IN MAIN REPO - THIS IS JUST FOR INFO
+	file1="/dev/nvme0n1"
+	file2="/var/plexguide/gce.check"
+	gcheck=$(dnsdomainname | tail -c 10)
+
+	file3="$(cat /var/plexguide/gce.done)"
+
+if [ "$file3" == 1 ];then 
+ echo " NVME are created "
+elif [ "$file3" != ];then
+	if [ -e "$file1" ] && [ ! -e "$file2" ] && [ "$gcheck" == ".internal" ]; then
+
+		  tee <<-EOF
+
+	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+	📂  Google Cloud Feeder Edition SET!
+	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+	⚡  Google Cloud Instance Detected!
+
+	⚠️  NOTE: Setting Up the NVME Drive For You! Please Wait!
+	⚠️  NOTE: Please don't close it !
+
+	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+	EOF
+		  apt-get install mdadm --no-install-recommends -yqq 2>&1 >>/dev/null
+		  export DEBIAN_FRONTEND=noninteractive
+		  #Check for NVME
+		  lsblk | grep nvme | awk '{print $1}' >/var/plexguide/nvme.log
+		  lsblk | grep nvme | awk '{print $1}' >/var/plexguide/nvmeraid.log
+		  sed -i 's/nvme0n//g' /var/plexguide/nvmeraid.log
+		  #Check for NVME
+		  nvme="$(tail -n1 /var/plexguide/nvmeraid.log)"
+
+		  if [[ "$nvme" == "2" ]]; then
+				mdadm --create /dev/md0 --level=0 --raid-devices=2 /dev/nvme0n1 /dev/nvme0n2
+				mkfs.ext4 -F /dev/md0
+				mkdir -p /mnt
+				mount /dev/md0 /mnt
+				sed -i '$ a\/dev/md0 /mnt ext4 discard,defaults,nobarrier,nofail 0 0' /etc/fstab
+				chown -cR 1000:1000 /mnt
+				tune2fs -m 0 /dev/md0
+				echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+				echo "✅ PASSED ! PG NVME RAID0 Creator with 2 NVMEs - finish"
+				echo "✅ PASSED ! HDD Space now :" $(df -h /mnt/ --total --local -x tmpfs | grep 'total' | awk '{print $2}')
+				echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+				echo -e "1" >/var/plexguide/gce.done
+				sleep 2
+		  elif [[ "$nvme" == "3" ]]; then
+				mdadm --create /dev/md0 --level=0 --raid-devices=3 /dev/nvme0n1 /dev/nvme0n2 /dev/nvme0n3
+				mkfs.ext4 -F /dev/md0
+				mkdir -p /mnt
+				mount /dev/md0 /mnt
+				sed -i '$ a\/dev/md0 /mnt ext4 discard,defaults,nobarrier,nofail 0 0' /etc/fstab
+				chown -cR 1000:1000 /mnt
+				tune2fs -m 0 /dev/md0
+				echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+				echo "✅ PASSED ! PG NVME RAID0 Creator with 3 NVMEs - finish"
+				echo "✅ PASSED ! HDD Space now :" $(df -h /mnt/ --total --local -x tmpfs | grep 'total' | awk '{print $2}')
+				echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+				echo -e "1" >/var/plexguide/gce.done
+				sleep 2
+		  elif [[ "$nvme" == "4" ]]; then
+				mdadm --create /dev/md0 --level=0 --raid-devices=4 /dev/nvme0n1 /dev/nvme0n2 /dev/nvme0n3 /dev/nvme0n4
+				mkfs.ext4 -F /dev/md0
+				mkdir -p /mnt
+				mount /dev/md0 /mnt
+				sed -i '$ a\/dev/md0 /mnt ext4 discard,defaults,nobarrier,nofail 0 0' /etc/fstab
+				chown -cR 1000:1000 /mnt
+				tune2fs -m 0 /dev/md0
+				echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+				echo "✅ PASSED ! PG NVME RAID0 Creator with 4 NVMEs - finish"
+				echo "✅ PASSED ! HDD Space now :" "$(df -h /mnt/ --total --local -x tmpfs | grep 'total' | awk '{print $2}')"
+				echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+				sleep 2
+				echo -e "1" >/var/plexguide/gce.done
+		  elif [[ "$nvme" == "1" ]]; then
+				sleep 3
+				mkfs.ext4 -F /dev/nvme0n1 1>/dev/null 2>&1
+				mount -o discard,defaults,nobarrier /dev/nvme0n1 /mnt
+				chmod a+w /mnt 1>/dev/null 2>&1
+				echo UUID=$(blkid | grep nvme0n1 | cut -f2 -d'"') /mnt ext4 discard,defaults,nobarrier,nofail 0 2 | tee -a /etc/fstab
+
+				mkdir -p /nvme1 1>/dev/null 2>&1
+				mkfs.ext4 -F /dev/nvme0n1
+				mount -o discard,defaults,nobarrier /dev/nvme0n1 /nvme1
+				chmod a+w /nvme1 1>/dev/null 2>&1
+				echo UUID=$(blkid | grep nvme0n1 | cut -f2 -d'"') /nvme1 ext4 discard,defaults,nobarrier,nofail 0 2 | tee -a /etc/fstab
+		  else
+				echo "nothing to do"
+		  fi
+
+		  touch /var/plexguide/gce.check
+		  rm -rf /var/plexguide/gce.failed 1>/dev/null 2>&1
+		  rm -rf /var/plexguide/gce.false 1>/dev/null 2>&1
+		  rm -rf /var/plexguide/nvme.log 1>/dev/null 2>&1
+		  rm -rf /var/plexguide/nvmeraid.log 1>/dev/null 2>&1
+
+		  echo "feeder" >/var/plexguide/pg.server.deploy
+		  cat /var/plexguide/pg.edition >/var/plexguide/pg.edition.stored
+
+		  tee <<-EOF
+
+	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+	📂  GCE Harddrive Deployed!
+	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+	⚡  Automatically Setting PG Google Feeder Edition (GCE)
+
+	⚠️  Please Wait!
+
+	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+	EOF
+
+		  sleep 6
+	elif [ ! -e "$file1" ] && [ ! -e "$file2" ] && [ "$gcheck" == ".internal" ]; then
+		  tee <<-EOF
+
+	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+	📂  Google Cloud Feeder Edition Failed!
+	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+	⚡  Google Cloud Instance Detected, but you Failed to setup an NVME
+	   drive per the wiki! This mistake only occurs on manual GCE
+	   deployments. Most likely you setup an SSD instead! The install will
+	   continue, but this will fail! Wipe the box and setup again with an
+	   NVME Drive!
+
+	━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+	EOF
+		  read -p 'Press [ENTER] to Continue! ' typed </dev/tty
+		  rm -rf /var/plexguide/gce.failed 1>/dev/null 2>&1
+		  rm -rf /var/plexguide/gce.false 1>/dev/null 2>&1
+		  rm -rf /var/plexguide/nvme.log 1>/dev/null 2>&1
+		  rm -rf /var/plexguide/nvmeraid.log1 >/dev/null 2>&1
+	else
+		  touch /var/plexguide/gce.false
+	fi
+fi
