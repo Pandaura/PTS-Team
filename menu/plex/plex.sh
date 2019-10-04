4842ae5bf4a9e9d192314e207e1c4725040ccf3a
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-10-04 20:59:57 +0200

    Update plex.sh

--- menu/plex/plex.sh
+++ menu/plex/plex.sh
@@ -25,7 +25,7 @@ question1() {
   tee <<-EOF
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-🌎 Plex Install 
+🌎 PG - PGBlitz Installer ~ http://plex.pgblitz.com
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 NOTE: DO NOT SELECT LOCAL SYSTEM for a REMOTE SERVER outside of your
 network as in using Hetzner, Google GCE, WholeSale Internet & Etc! If you
@@ -66,7 +66,10 @@ this step as you won't need to claim it again.
 EOF
 
   read -p 'Plex Server Claim Number | Press [ENTER]: ' typed </dev/tty
-  echo $typed >/var/plexguide/plex.claim && break=on
+  echo $typed >/var/plexguide/plex.claim 
+  if [ $(cat /var/plexguide/image/plex) == linuxserver/plex ];then
+  bash /opt/plexguide/menu/plex/lsio-plex-claim.sh $(cat /var/plexguide/plex.claim) 
+  fi
 }
 
 # FUNCTIONS END ##############################################################
