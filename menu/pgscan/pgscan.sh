0c0682962a45fe41893d9a149d6bcdbe6ec49644
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-10-06 23:43:31 +0200

    Update pgscan.sh

--- menu/pgscan/pgscan.sh
+++ menu/pgscan/pgscan.sh
@@ -6,14 +6,6 @@
 # GNU:        General Public License v3.0
 ################################################################################
 
-#!/bin/bash
-#
-# Title:      PGBlitz (Reference Title File)
-# Author(s):  Admin9705
-# URL:        https://pgblitz.com - http://github.pgblitz.com
-# GNU:        General Public License v3.0
-################################################################################
-
 # KEY VARIABLE RECALL & EXECUTION
 mkdir -p /var/plexguide/pgscan
 
