21f664bf7f641f467614473c341141ec2d374af2
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-10-13 19:59:56 +0200

    Update localspace.sh

--- menu/pgui/templates/localspace.sh
+++ menu/pgui/templates/localspace.sh
@@ -8,14 +8,6 @@
 ################################################################################
 
 # Starting Actions
-#networktools | vnstat | vnstati install | please wait"
-apt-get install ethtool vnstat vnstati -yqq 2>&1 >>/dev/null
-network="$(ifconfig | grep -E 'eno1|enp|ens5' | awk '{print $1}' | sed -e 's/://g')"
-sed -i 's/eth0/'$network'/g' /etc/vnstat.conf
-sed -i 's/UnitMode 0/UnitMode 1/g' /etc/vnstat.conf
-sed -i 's/RateUnit 1/RateUnit 0/g' /etc/vnstat.conf
-sed -i 's/Locale "-"/Locale "LC_ALL=en_US.UTF-8"/g' /etc/vnstat.conf
-/etc/init.d/vnstat restart 2>&1 >>/dev/null
 
 startscript() {
 
@@ -29,7 +21,6 @@ startscript() {
                 du -sh /mnt/downloads | awk '{print $1}' >>/var/plexguide/spaceused.log
 
                 sleep 60
-
         done
 
 }
