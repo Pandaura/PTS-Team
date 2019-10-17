f6fcc9b2699f21b45b0f967d4ac6f64f42df6944
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-10-17 14:23:12 +0200

    Update autobackup.sh

--- menu/pgvault/autobackup.sh
+++ menu/pgvault/autobackup.sh
@@ -1,4 +1,4 @@
-source /opt/pgvault/autobackup.func
+source /opt/plexguide/menu/pgvault/autobackup.func
 file="/var/plexguide/restore.id"
 if [ ! -e "$file" ]; then
   echo "[NOT-SET]" >/var/plexguide/restore.id
