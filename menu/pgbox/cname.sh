2dd96047cdd872578437813531f927fb13f3978e
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-10-22 23:30:49 +0200

    revert cname to original

--- menu/pgbox/cname.sh
+++ menu/pgbox/cname.sh
@@ -20,9 +20,11 @@ question1() {
     cname=$(cat "/var/plexguide/$program.cname")
     port=$(cat "/var/plexguide/$program.port")
     tee <<-EOF
+
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ⌛ $program - Set subdomains & ports
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
 EOF
     if [[ $port != "" ]]; then
         tee <<-EOF
@@ -35,7 +37,7 @@ EOF
     fi
 
     tee <<-EOF
-    
+
 [1] Change subdomain
 [2] Change external port
 
@@ -43,9 +45,7 @@ EOF
 
     if [[ $port != "" ]]; then
         tee <<-EOF
-        
 [A] Use https://$cname.$domain:$port
-
 EOF
     else
         tee <<-EOF
@@ -53,7 +53,9 @@ EOF
 EOF
     fi
     tee <<-EOF
+
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
 EOF
 
     read -p '↘️  Type Number | Press [ENTER]: ' typed </dev/tty
@@ -69,9 +71,9 @@ EOF
                 badinput1
             else
                 echo "$typed" >"/var/plexguide/$program.cname"
+                question1
             fi
         fi
-        question1
     elif [ "$typed" == "2" ]; then
         read -p "🌍 Type port 1025-65535 to use for $program | blank for default | Press [ENTER]: " typed </dev/tty
         if [[ "$typed" == "" ]]; then
