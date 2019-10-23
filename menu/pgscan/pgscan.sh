d2d85c84ed45e393f83b8da2f020a52340256ea2
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-10-23 23:56:18 +0200

    update pgscan alias

--- menu/pgscan/pgscan.sh
+++ menu/pgscan/pgscan.sh
@@ -122,7 +122,7 @@ PAS_CONFIG="/opt/appdata/pgscan/config/config.json"
 
 "Your Plex Autoscan URL:"
 
-"https://${SUBDOMAIN}:${SERVER_PORT}/${SERVER_PASS}"
+"https://${SERVER_IP}:${SERVER_PORT}/${SERVER_PASS}"
 
 Press Enter to Exit
 
