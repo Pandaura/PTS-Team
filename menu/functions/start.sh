c87a9f39738e80f57b1a5dbc1f28cfa9969575d1
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-10-24 19:28:56 +0200

    Update start.sh

--- menu/functions/start.sh
+++ menu/functions/start.sh
@@ -22,12 +22,11 @@ EOF
 }
 
 downloadpg() {
-  rm -rf /opt/plexguide 1>/dev/null 2>&1  
-  ansible-playbook /opt/pgstage/clone.yml 
+  rm -rf /opt/plexguide
+  git clone --single-branch https://github.com/MrDoobPG/PGBlitz.com.git /opt/plexguide  1>/dev/null 2>&1
+  ansible-playbook /opt/plexguide/menu/alias/alias.yml  1>/dev/null 2>&1
   rm -rf /opt/plexguide/place.holder >/dev/null 2>&1
   rm -rf /opt/plexguide/.git* >/dev/null 2>&1
-  ansible-playbook /opt/plexguide/menu/alias/alias.yml 1>/dev/null 2>&1
-  touch /var/plexguide/exited.upgrade
 }
 
 missingpull() {
