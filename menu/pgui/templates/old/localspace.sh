15d6162c9c8c52f4f1566a198eb92593e428aba3
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-11-08 12:21:26 +0100

    UI major update
    image chnage and some fixes
    new Image LSIO Nginx
    php7 support
    log support
    more performance

---
+++ menu/pgui/templates/old/localspace.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Title:      PTS Community 
+# Author:     MrDoob
+# URL:        WTFH >-!-< why you need this ^^ 
+# GNU:        General Public License v3.0
+#
+################################################################################
+
+# Starting Actions
+
+startscript() {
+
+        while [ 1 ]; do
+
+                rm -rf /var/plexguide/spaceused.log
+
+                # move and downloads for the UI
+
+                du -sh /mnt/move | awk '{print $1}' >>/var/plexguide/spaceused.log
+                du -sh /mnt/downloads | awk '{print $1}' >>/var/plexguide/spaceused.log
+
+                sleep 60
+        done
+
+}
+
+# keeps the function in a loop
+cheeseballs=0
+while [[ "$cheeseballs" == "0" ]]; do startscript; done
