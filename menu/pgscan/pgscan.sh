5bc3b15c89a09eb7bd1ac668ee756c4371696868
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-11-21 03:48:04 +0100

    hrrr

--- menu/pgscan/pgscan.sh
+++ menu/pgscan/pgscan.sh
@@ -22,19 +22,19 @@ userstatus() {
     ustatus="✅ DEPLOYED"
   else ustatus="⚠️ NOT DEPLOYED"; fi
 }
+
 tokenstatus() {
   ptokendep=$(cat /var/plexguide/pgscan/plex.token)
+ PGSELFTEST=$(curl -LI "http://localhost:32400/system?X-Plex-Token=$(cat /opt/plex_autoscan/config/config.json | jq .PLEX_TOKEN | sed 's/"//g')"  -o /dev/null -w '%{http_code}\n' -s)
+
   if [ "$ptokendep" != "" ]; then
-   if [[ -f "/opt/plex_autoscan/config/config.json" ]]; then
-   		  pstatus="❌ DEPLOYED BUT PAS CONFIG IS MISSING"
-	##second check
-	PGSELFTEST=$(curl -LI "http://localhost:32400/system?X-Plex-Token=$(cat /opt/plex_autoscan/config/config.json | jq .PLEX_TOKEN | sed 's/"//g')"  -o /dev/null -w '%{http_code}\n' -s)
-		if [[ $PGSELFTEST -ge 200 && $PGSELFTEST -le 299 ]]; then
-		  pstatus="✅ DEPLOYED"
-		  else
-		  pstatus="❌ DEPLOYED BUT PAS TOKEN FAILED"
-		fi
-	fi
+        if [ ! -f "/opt/plex_autoscan/config/config.json" ]; then
+                pstatus="❌ DEPLOYED BUT PAS CONFIG MISSING";
+        elif [[ $PGSELFTEST -ge 200 && $PGSELFTEST -le 299 ]]; then
+                pstatus="✅ DEPLOYED"
+        else
+         pstatus="❌ DEPLOYED BUT PAS TOKEN FAILED"
+        fi
   else pstatus="⚠️ NOT DEPLOYED"; fi
 }
 
