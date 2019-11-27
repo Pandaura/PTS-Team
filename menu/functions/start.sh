ca5a8a58a065cc776b93a7ef1cdf4863ae171cc1
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-11-27 21:42:23 +0100

    Update start.sh

--- menu/functions/start.sh
+++ menu/functions/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# Title:      PGBlitz (Reference Title File)
-# Author(s):  Admin9705 - Deiteq
-# URL:        https://pgblitz.com - http://github.pgblitz.com
+# Title:      PTS major file
+# org.Author(s):  Admin9705 - Deiteq
+# Mod from MrDoob for PTS
 # GNU:        General Public License v3.0
 ################################################################################
 source /opt/plexguide/menu/functions/functions.sh
