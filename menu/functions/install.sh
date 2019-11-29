576535b1aea952f341a0c4a8d894a678dc258b20
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-11-29 13:38:22 +0100

    Update install.sh

--- menu/functions/install.sh
+++ menu/functions/install.sh
@@ -6,6 +6,7 @@
 # GNU:        General Public License v3.0
 ################################################################################
 source /opt/plexguide/menu/functions/functions.sh
+source /opt/plexguide/menu/functions/serverid.sh
 
 updateprime() {
   abc="/var/plexguide"
@@ -310,6 +311,6 @@ dockerinstall() {
     ansible-playbook /opt/plexguide/menu/pg.yml --tags docker
 }
 
-serverid() {
-   bash /opt/plexguide/menu/functions/serverid.sh
-}
+#serverid() {
+#   bash /opt/plexguide/menu/functions/serverid.sh
+#}
