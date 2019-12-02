511d1cebdb0dc166a3242c11ea1b679be7678e64
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-12-02 23:49:24 +0100

    Update gtused.sh

--- menu/pgui/templates/gtused.sh
+++ menu/pgui/templates/gtused.sh
@@ -10,9 +10,12 @@ log="/var/plexguide"
 useragent="$(cat /var/plexguide/uagent)"
 
 #if else loop for checking what is running
+    rm -f $log/gdcrypt.log && touch $log/gdcrypt.log
+    rm -f $log/tdcrypt.log && touch $log/tdcrypt.log
+    rm -f $log/gduncrypt.log && touch $log/gduncrypt.log
+    rm -f $log/tduncrypt.log && touch $log/tduncrypt.log
 
 if grep -q "gdrive:" $config; then
-    rm -f $log/gduncrypt.log && touch $log/gduncrypt.log
     rclone size gdrive: \
         --verbose=1 \
         --fast-list \
@@ -21,14 +24,13 @@ if grep -q "gdrive:" $config; then
         --user-agent="$useragent" \
         --timeout=30m \
         --exclude="**encrypt**" \
-        --config /opt/appdata/plexguide/rclone.conf | awk '{print $3,$4}' >>$log/gduncrypt.log
+        --config /opt/appdata/plexguide/rclone.conf | awk '{print $3,$4}' >$log/gduncrypt.log
     sed -i 's/Total size: / /g' $log/gduncrypt.log
 else echo "🔴 NOT DEPLOYED" $log/gduncrypt.log; fi
 
 sleep 2
 
 if grep -q "tdrive:" $config; then
-    rm -f $log/tduncrypt.log && touch $log/tduncrypt.log
     rclone size tdrive: \
         --verbose=1 \
         --fast-list \
@@ -37,14 +39,13 @@ if grep -q "tdrive:" $config; then
         --user-agent="$useragent" \
         --timeout=30m \
         --exclude="**encrypt**" \
-        --config /opt/appdata/plexguide/rclone.conf | awk '{print $3,$4}' >>$log/tduncrypt.log
+        --config /opt/appdata/plexguide/rclone.conf | awk '{print $3,$4}' >$log/tduncrypt.log
     sed -i 's/Total size: / /g' $log/tduncrypt.log
 else echo "🔴 NOT DEPLOYED" $log/tduncrypt.log; fi
 
 sleep 2
 
 if grep -q "gcrypt:" $config; then
-    rm -f $log/gdcrypt.log && touch $log/gdcrypt.log
     rclone size gcrypt: \
         --verbose=1 \
         --fast-list \
@@ -52,14 +53,13 @@ if grep -q "gcrypt:" $config; then
         --user-agent="$useragent" \
         --no-update-modtime \
         --timeout=30m \
-        --config /opt/appdata/plexguide/rclone.conf | awk '{print $3,$4}' >>$log/gdcrypt.log
+        --config /opt/appdata/plexguide/rclone.conf | awk '{print $3,$4}' >$log/gdcrypt.log
     sed -i 's/Total size: / /g' $log/gdcrypt.log
 else echo "🔴 NOT DEPLOYED" $log/gdcrypt.log; fi
 
 sleep 2
 
 if grep -q "tcrypt:" $config; then
-    rm -f $log/tdcrypt.log && touch $log/tdcrypt.log
     rclone size tcrypt: \
         --verbose=1 \
         --fast-list \
@@ -67,6 +67,6 @@ if grep -q "tcrypt:" $config; then
         --user-agent="$useragent" \
         --no-update-modtime \
         --timeout=30m \
-        --config /opt/appdata/plexguide/rclone.conf | awk '{print $3,$4}' >>$log/tdcrypt.log
+        --config /opt/appdata/plexguide/rclone.conf | awk '{print $3,$4}' >$log/tdcrypt.log
     sed -i 's/Total size: / /g' $log/tdcrypt.log
 else echo "🔴 NOT DEPLOYED" $log/tdcrypt.log; fi
