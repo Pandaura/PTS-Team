714b3386d9e3416e714d97839470b52ad38a384a
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-12-07 17:48:42 +0100

    set traktarr limit to 50

--- menu/traktarr/traktarr-list/traktarr.sh
+++ menu/traktarr/traktarr-list/traktarr.sh
@@ -1,42 +1,42 @@
 #!/bin/bash 
 
-traktarr movies -t https://trakt.tv/users/porkie16/lists/docu-movies
+traktarr movies -t https://trakt.tv/users/porkie16/lists/docu-movies -l 50
 sleep 30s
-traktarr movies -t https://trakt.tv/users/porkie16/lists/thriller-movies-rating-65
+traktarr movies -t https://trakt.tv/users/porkie16/lists/thriller-movies-rating-65 -l 50
 sleep 30s
-traktarr movies -t https://trakt.tv/users/porkie16/lists/horror-movies-rating-65
+traktarr movies -t https://trakt.tv/users/porkie16/lists/horror-movies-rating-65 -l 50
 sleep 30s
-traktarr movies -t https://trakt.tv/users/porkie16/lists/sci-fi-movies-65-rating
+traktarr movies -t https://trakt.tv/users/porkie16/lists/sci-fi-movies-65-rating -l 50
 sleep 30s
-traktarr movies -t https://trakt.tv/users/porkie16/lists/comedy-movies-65-rating
+traktarr movies -t https://trakt.tv/users/porkie16/lists/comedy-movies-65-rating -l 50
 sleep 30s
-traktarr movies -t https://trakt.tv/users/porkie16/lists/animation-movies-kids-only-rating-65
+traktarr movies -t https://trakt.tv/users/porkie16/lists/animation-movies-kids-only-rating-65 -l 50
 sleep 30s
-traktarr movies -t https://trakt.tv/users/porkie16/lists/action-movies-rating-65-rating
+traktarr movies -t https://trakt.tv/users/porkie16/lists/action-movies-rating-65-rating -l 50
 sleep 30s
-traktarr shows -t https://trakt.tv/users/porkie16/lists/disney-tv-shows-65-rating
+traktarr shows -t https://trakt.tv/users/porkie16/lists/disney-tv-shows-65-rating -l 50
 sleep 30s
-traktarr movies -t https://trakt.tv/users/enormoz/lists/netflix-movies
+traktarr movies -t https://trakt.tv/users/enormoz/lists/netflix-movies -l 50
 sleep 30s
-traktarr movies -t https://trakt.tv/users/theultimatec0der/lists/all-movies-2019
+traktarr movies -t https://trakt.tv/users/theultimatec0der/lists/all-movies-2019 -l 50
 sleep 30s
-traktarr movies -t https://trakt.tv/users/theultimatec0der/lists/all-movies-2018
+traktarr movies -t https://trakt.tv/users/theultimatec0der/lists/all-movies-2018 -l 50
 sleep 30s
-traktarr shows -t https://trakt.tv/users/theultimatec0der/lists/all-shows-2019
+traktarr shows -t https://trakt.tv/users/theultimatec0der/lists/all-shows-2019 -l 50
 sleep 30s
-traktarr shows -t https://trakt.tv/users/theultimatec0der/lists/all-shows-2018
+traktarr shows -t https://trakt.tv/users/theultimatec0der/lists/all-shows-2018 -l 50
 sleep 30s
-traktarr shows -t https://trakt.tv/users/theultimatec0der/lists/all-netflix-series
+traktarr shows -t https://trakt.tv/users/theultimatec0der/lists/all-netflix-series -l 50
 sleep 30s
-traktarr shows -t https://trakt.tv/users/theultimatec0der/lists/all-hulu-series
+traktarr shows -t https://trakt.tv/users/theultimatec0der/lists/all-hulu-series -l 50
 sleep 30s
-traktarr shows -t https://trakt.tv/users/theultimatec0der/lists/all-amazon-series
+traktarr shows -t https://trakt.tv/users/theultimatec0der/lists/all-amazon-series -l 50
 sleep 30s
-traktarr movies -t https://trakt.tv/users/pntjr/lists/upcoming-movies
+traktarr movies -t https://trakt.tv/users/pntjr/lists/upcoming-movies -l 50
 sleep 30s
-traktarr shows -t https://trakt.tv/users/porkie16/lists/german-tv-shows
+traktarr shows -t https://trakt.tv/users/porkie16/lists/german-tv-shows -l 50
 sleep 30s
-traktarr movies -t https://trakt.tv/users/mrdoob/lists/mylist
+traktarr movies -t https://trakt.tv/users/mrdoob/lists/mylist -l 50
 sleep 30s
 
 exit 0
