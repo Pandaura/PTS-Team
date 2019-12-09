384481ec434be4936e39d5e9996303acdb9b5461
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-12-09 20:31:15 +0100

    Update plex.sh

--- menu/plex/plex.sh
+++ menu/plex/plex.sh
@@ -54,10 +54,10 @@ EOF
   read -p '↘️  Type Number | Press [ENTER]: ' typed </dev/tty
 
   case $typed in
-  echo ""
   1) echo "remote" >/var/plexguide/plex.server ;;
   2) echo "local" >/var/plexguide/plex.server ;;
   *) badinput && setserver ;;
+  esac
 }
 
 # THIRD QUESTION
