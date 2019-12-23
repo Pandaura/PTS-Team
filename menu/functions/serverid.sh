36c54450feacc49a3edae0b99e704531c7f3ac96
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-12-23 19:37:50 +0100

    patch serverid.sh for vault

--- menu/functions/serverid.sh
+++ menu/functions/serverid.sh
@@ -35,6 +35,12 @@ EOF
 EOF
     echo "$typed" >${abc}/server.id
 	recreatefolder
+    tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+✅️  PASS: Backup folder created on GDrive 
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
     sleep 1
   fi
 }
@@ -66,15 +72,21 @@ EOF
 EOF
     echo "$typed" >${abc}/server.id
 	recreatefolder
+    tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+✅️  PASS: Backup folder created on GDrive 
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
     sleep 1
   fi
 }
 
 recreatefolder() {
-        start=$(cat /var/plexguide/server.id)
-        serveridcreate=$(tree -d -L 1 /mnt/gdrive/plexguide/backup | awk '{print $2}' | tail -n +2 | head -n -2 | grep "$(cat /var/plexguide/server.id)")
-        if [[ $start != $serveridcreate ]]; then
-        rclone --config /opt/appdata/plexguide/rclone.conf mkdir gdrive:/plexguide/backup/$(cat /var/plexguide/server.id);fi
+start=$(cat /var/plexguide/server.id)
+serveridcreate=$(tree -d -L 1 /mnt/gdrive/plexguide/backup | awk '{print $2}' | tail -n +2 | head -n -2 | grep "$(cat /var/plexguide/server.id)")
+if [[ $start != $serveridcreate ]]; then
+rclone mkdir gdrive:/plexguide/backup/$(cat /var/plexguide/server.id) --config /opt/appdata/plexguide/rclone.conf;fi
 }
 
 
--- menu/interface/serverid.sh
+++ menu/interface/serverid.sh
@@ -13,7 +13,7 @@ abc="/var/plexguide"
 
 serverid() {
 
-if [ "$start" != "$stored" ]; then
+if [[ "$start" != "$stored" ]]; then
 
  tee <<-EOF
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -22,7 +22,7 @@ if [ "$start" != "$stored" ]; then
 EOF
   read -p '🌏  TYPE Server ID | Press [ENTER]: ' typed </dev/tty
 
-  if [ "$typed" == "" ]; then
+  if [[ "$typed" == "" ]]; then
     tee <<-EOF
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ⛔️  WARNING! - The Server ID Cannot Be Blank!
@@ -41,11 +41,10 @@ EOF
     # Prevents From Repeating
     echo "$typed" >${abc}/server.id
     cat ${abc}/server.id >${abc}/server.id.stored
-        start=$(cat /var/plexguide/server.id)
-        server_id=$(cat /var/plexguide/server.id)
-        serveridcreate=$(tree -d -L 1 /mnt/gdrive/plexguide/backup | awk '{print $2}' | tail -n +2 | head -n -2 | grep "$(cat /var/plexguide/server.id)")
-        if [[ $start != $serveridcreate ]]; then
-        rclone --config /opt/appdata/plexguide/rclone.conf mkdir gdrive:/plexguide/backup/${server_id};fi
+         start=$(cat /var/plexguide/server.id)
+         serveridcreate=$(tree -d -L 1 /mnt/gdrive/plexguide/backup | awk '{print $2}' | tail -n +2 | head -n -2 | grep "$(cat /var/plexguide/server.id)")
+         if [[ $start != $serveridcreate ]]; then
+         rclone mkdir gdrive:/plexguide/backup/$(cat /var/plexguide/server.id) --config /opt/appdata/plexguide/rclone.conf; fi
     sleep 3
   fi
 
