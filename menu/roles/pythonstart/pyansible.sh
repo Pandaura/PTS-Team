0671b340466e7aec87d06ab81c190b5f12e41e19
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2020-01-21 18:01:01 +0100

    [ BUG ] Python rebase

--- menu/roles/pythonstart/pyansible.sh
+++ menu/roles/pythonstart/pyansible.sh
@@ -1,7 +1,4 @@
-  ansible="2.8.2"
-  pip="19.1.1"
-	
-  apt-get install -y --reinstall \
+apt-get install -y --reinstall \
   nano \
   git \
   build-essential \
@@ -13,37 +10,47 @@
   python3-testresources \
   python-dev \
   python-pip
-  python3 -m pip install --disable-pip-version-check --upgrade --force-reinstall pip==${pip}
-  python3 -m pip install --disable-pip-version-check --upgrade --force-reinstall setuptools
-  python3 -m pip install --disable-pip-version-check --upgrade --force-reinstall \
-    pyOpenSSL \
-    requests \
-    netaddr \
-    pipenv
-  python -m pip install --disable-pip-version-check --upgrade --force-reinstall pip==${pip}
-  python -m pip install --disable-pip-version-check --upgrade --force-reinstall setuptools
-  python -m pip install --disable-pip-version-check --upgrade --force-reinstall ansible==${1-$ansible}
+python3 -m pip install --disable-pip-version-check --upgrade --force-reinstall pip==19.1.1
+python3 -m pip install --disable-pip-version-check --upgrade --force-reinstall setuptools
+python3 -m pip install --disable-pip-version-check --upgrade --force-reinstall \
+  pyOpenSSL \
+  requests \
+  netaddr \
+  lxml
+python -m pip install --disable-pip-version-check --upgrade --force-reinstall pip==19.1.1
+python -m pip install --disable-pip-version-check --upgrade --force-reinstall setuptools
+python -m pip install --disable-pip-version-check --upgrade --force-reinstall \
+  pyOpenSSL \
+  requests \
+  netaddr \
+  lxml
+python -m pip install --disable-pip-version-check --upgrade --force-reinstall ansible==${1-2.8.2}
 
-  pip install -U --force-reinstall pip==9.0.3
-  pip3 install -U --force-reinstall pip==9.0.3
-  pip install --upgrade --force-reinstall pip==9.0.3
-  pip install --upgrade pip
-  pip install docker-py --ignore-installed PyYAML
+pip install -U --force-reinstall pip==9.0.3
+pip3 install -U --force-reinstall pip==9.0.3
+pip install --upgrade --force-reinstall pip==9.0.3
+pip install --upgrade pip
+pip install docker-py --ignore-installed PyYAML
 
-  ## Copy pip to /usr/bin
-  cp /usr/local/bin/pip /usr/bin/pip
-  cp /usr/local/bin/pip3 /usr/bin/pip3
 
-  mkdir -p /etc/ansible/inventories/ 1>/dev/null 2>&1
-  echo "[local]" >/etc/ansible/inventories/local
-  echo "127.0.0.1 ansible_connection=local" >>/etc/ansible/inventories/local
+## Copy pip to /usr/bin
+cp /usr/local/bin/pip /usr/bin/pip
+cp /usr/local/bin/pip3 /usr/bin/pip3
 
-  ### Reference: https://docs.ansible.com/ansible/2.4/intro_configuration.html
-  echo "[defaults]" >/etc/ansible/ansible.cfg
-  echo "deprecation_warnings=False" >>/etc/ansible/ansible.cfg
-  echo "command_warnings = False" >>/etc/ansible/ansible.cfg
-  echo "callback_whitelist = profile_tasks" >>/etc/ansible/ansible.cfg
-  echo "inventory = /etc/ansible/inventories/local" >>/etc/ansible/ansible.cfg
+mkdir -p /etc/ansible/inventories/ 1>/dev/null 2>&1
+echo "[local]" >/etc/ansible/inventories/local
+echo "127.0.0.1 ansible_connection=local" >>/etc/ansible/inventories/local
 
-  # Variables Need to Line Up with pg.sh (start)
-  touch /var/plexguide/background.1
+### Reference: https://docs.ansible.com/ansible/2.4/intro_configuration.html
+echo "[defaults]" >/etc/ansible/ansible.cfg
+echo "deprecation_warnings=False" >>/etc/ansible/ansible.cfg
+
+echo "command_warnings = False" >>/etc/ansible/ansible.cfg
+echo "callback_whitelist = profile_tasks" >>/etc/ansible/ansible.cfg
+echo "inventory = /etc/ansible/inventories/local" >>/etc/ansible/ansible.cfg
+
+# Remove Cows when cowsay installed on main system
+echo "nocows = 1" >>/etc/ansible/ansible.cfg
+
+# Variables Need to Line Up with pg.sh (start)
+touch /var/plexguide/background.1
