044b60d1785c08ebf0609df6fca31074d5cca115
hawkinzzz <hawkinzzz@hotmail.co.uk>
2020-01-25 17:06:23 +0000

    Update docker user rule

--- menu/pgscan/pgscan.sh
+++ menu/pgscan/pgscan.sh
@@ -315,8 +315,8 @@ EOF
 }
 pversion() {
 plexcontainerversion=$(docker ps --format '{{.Image}}' | grep "plex")
-  if [[ "$plexcontainerversion" == "linuxserver/plex:latest" ]]; then
-      echo -e "abc" >/var/plexguide/pgscan/plex.dockeruserset
+  if [[ "$plexcontainerversion" == "plexinc/pms-docker" ]]; then
+      echo -e "plex" >/var/plexguide/pgscan/plex.dockeruserset
    else echo "abc" >/var/plexguide/pgscan/plex.dockeruserset
 fi
 pasuserdocker=$(cat /var/plexguide/pgscan/plex.dockeruserset)
